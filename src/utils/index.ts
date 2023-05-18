0f0c5fed632f79d113c177a9376e2556f8043bc9
sandro <devspuig@pm.me>
2023-05-18 10:45:00 +0200

    Add docs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,8 @@
 import { cometConstants } from '@compound-finance/compound-js/dist/nodejs/constants';
 import { Contract, providers, Wallet } from 'ethers';
-import { Comet_ABI } from '../types/comet';
-import { AssetInfo } from '@compound-finance/compound-js/dist/nodejs/types';
-import { Collateral, Collaterals } from '../types';
 import { WidoCollateralSwap_ABI } from '../types/widoCollateralSwap';
+import { Comet_ABI } from '../types/comet';
+import { Collateral, Collaterals } from '../types';
 
 const MAINNET_ID = 1;
 const POLYGON_ID = 137;
@@ -18,22 +17,40 @@ const widoCollateralSwapAddress: Record<number, string> = {
   [POLYGON_ID]: "",
 }
 
+/**
+ * Get contract address from the comet key
+ * @param cometKey
+ */
 export function getCometAddress(cometKey: string): string {
   // @ts-ignore
   return cometConstants.address[cometKey].Comet;
 }
 
+/**
+ * Convert comet key to chain ID
+ * @param comet
+ */
 export function getChainId(comet: string): number {
   const chainKey = comet.split("_")[0]
   // @ts-ignore
   return keyToId[chainKey];
 }
 
+/**
+ * Builds an instance of the Comet contract given the comet key
+ * @param comet
+ * @param provider
+ */
 export function getCometContract(comet: string, provider: providers.Provider): Contract {
   const cometAddress = getCometAddress(comet)
   return new Contract(cometAddress, Comet_ABI, provider);
 }
 
+/**
+ * Builds an instance of the Wido contract on a given chain
+ * @param chainId
+ * @param signer
+ */
 export function getWidoContract(chainId: number, signer: Wallet): Contract {
   if (!(chainId in widoCollateralSwapAddress)) {
     throw new Error(`WidoCollateralSwap not deployed on chain ${chainId}`);
@@ -42,10 +59,11 @@ export function getWidoContract(chainId: number, signer: Wallet): Contract {
   return new Contract(address, WidoCollateralSwap_ABI, signer);
 }
 
-export async function getAssetInfoByAddress(contract: Contract, address: string): Promise<AssetInfo> {
-  return await contract.callStatic.getAssetInfoByAddress(address);
-}
-
+/**
+ * Seek and return a collateral from the list, by address
+ * @param collaterals
+ * @param address
+ */
 export function pickAsset(collaterals: Collaterals, address: string): Collateral {
   for (const collateral of collaterals) {
     if (collateral.address === address) {
