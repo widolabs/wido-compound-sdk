eac04191143cd9576a97dec38fedb3699475240e
sandro <devspuig@pm.me>
2023-05-24 07:57:10 +0200

    Add polygon address

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,7 +22,7 @@ const keyToId = {
 
 export const widoCollateralSwapAddress: Record<number, string> = {
   [MAINNET_ID]: "0x5E5713a0d915701F464DEbb66015adD62B2e6AE9",
-  [POLYGON_ID]: "",
+  [POLYGON_ID]: "0x5E5713a0d915701F464DEbb66015adD62B2e6AE9",
 }
 
 /**
